ec3ca316955010e71638d23ba1d4d967a130c53d
Shadman Ahmed <shadman@outlook.com>
2020-05-12 06:16:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,11 +2,11 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def method_name(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def method_name(name, language = "Ruby")
+def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
